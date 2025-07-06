8f7e389f3a3e2c381e526c526a2990d6e84a132f
sarveshhalade <sdhalade9799@gmail.com>
2025-07-06 13:21:09 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+console.log(3 + 3)
+
+console.log("Sarvesh")
+
+let name = "sarvesh"
+let age = 21
+let isloggedin = false
+
+
+//string
+//bigint
+//number
+//boolean = true , false
+//null = standalone value
+//symbol = unique
+
+//object
+
+console.log(typeof "sarvesh");
+console.log(typeof age);
+console.log(typeof null);
