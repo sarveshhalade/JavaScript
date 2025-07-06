e0fd2da9c583dc234f4a679974753563ebfbfd33
sarveshhalade <sdhalade9799@gmail.com>
2025-07-06 11:14:54 +0530

    constant&variable

---
+++ constant.js
@@ -0,0 +1,15 @@
+const accountId = 12387
+let accountEmail = "sarvesh9997@gmail.com"
+var accountPassword = "1298"
+accountCity = "KOP"
+
+//constant remain as it is they cannot be change ,varibale can be changed 
+
+accountEmail = "sdh9997@.com"
+accountPassword = "3873"
+accountCity = "MUMbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+//This will print it in table format
+
+//Throughtout the program we have to variable as "let" not as "var" because of issue in block scope and functional scope
