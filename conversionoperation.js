68761c76662456f871c6db3c425a0fe06cc1be5a
sarveshhalade <sdhalade9799@gmail.com>
2025-07-08 11:40:49 +0530

    conversion of datatypes

---
+++ conversionoperation.js
@@ -0,0 +1,16 @@
+let score = "95adc"
+
+//console.log(typeof score)
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+ 
+//95 > 95
+//"95adc" > NaN
+//true > 1 , false > 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn =  Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
