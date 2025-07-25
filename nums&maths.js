40a1ad3f6de772596d4822addd0995cf2bc0531e
sarveshhalade <sdhalade9799@gmail.com>
2025-07-25 20:40:18 +0530

    nums maths

---
+++ nums&maths.js
@@ -0,0 +1,35 @@
+const score = 3000
+// console.log(score);
+
+// const balance = new Number(3000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 12.87
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+//**********************MATH**************************
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(625));
+console.log(Math.random()); //random No lies between 0 to 1
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
