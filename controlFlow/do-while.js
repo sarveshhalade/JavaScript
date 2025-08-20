1e3a58e8ea26ceb7490dd767c0e7744f9df1c2f6
sarveshhalade <sdhalade9799@gmail.com>
2025-08-21 00:04:14 +0530

    do-while

---
+++ controlFlow/do-while.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <= 10) {
+    //console.log(`value : ${index}`);
+    index = index + 2;
+}
+
+let myArray =["mom", "dad", "brother", "sister"]
+let arr = 0
+while (arr < myArray.length) {
+   console.log(`value is :${myArray[arr]}`);
+   
+    arr++;
+    
+}
+
+let score = 1
+do {
+    console.log(`vlaue is : ${score}`);
+    score++;
+} while (score <= 10);
