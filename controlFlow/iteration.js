ab33ca109f410036ddca44554f322b3d8b0cafdb
sarveshhalade <sdhalade9799@gmail.com>
2025-08-21 00:05:36 +0530

    itteration

---
+++ controlFlow/iteration.js
@@ -0,0 +1,33 @@
+// for
+for (let index = 1; index < 10; index++) {
+    const element = index;
+    if(element == 5){
+        //console.log("5 is a best number");
+           }
+  //  console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+//console.log(`Outer loop vlaue: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+   // console.log(i + '*' + j + '=' + i*j);
+    
+}
+  }
+
+
+  let myArray =["apple", "banana", "cherry"]
+  for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+  }
+
+  for (let i = 1; i < 20; i++) {
+    if(i == 5){
+        console.log(`detected 5`);
+        //break 
+        continue
+    }
+       console.log(`Value: ${i}`);
+  }
